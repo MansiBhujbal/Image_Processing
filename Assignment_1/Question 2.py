f1c7eb53e8bfe591cc78e4c3c6dfa3861bf94672
MansiBhujbal <66293553+MansiBhujbal@users.noreply.github.com>
2020-06-04 14:05:20 +0530

    Add files via upload

---
+++ Assignment_1/Question 2.py
@@ -0,0 +1,17 @@
+#To display a vertically flipped frame after ‘n’ vertically upright frames, from live webcam feed.
+import cv2 
+cap = cv2.VideoCapture(0)
+counter=0
+while True :
+    x,frame=cap.read()
+    #cv2.imshow('Image',frame) 
+    counter +=1
+    flipped=cv2.flip(frame,1)
+    
+    if counter%2 == 0 :
+        cv2.imshow('Image',frame)
+    else :
+        cv2.imshow('Image',flipped)
+
+    if cv2.waitKey(2000) & 0xFF == ord('q') :
+        break 
