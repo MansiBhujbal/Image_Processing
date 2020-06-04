20999891fd4fe7e0ee94b7a064f6aa4588362e33
Mansi Bhujbal <mansibhujbal24@gmail.com>
2020-06-04 13:32:48 +0530

    [Uploading question 2]

---
+++ Q2..py
@@ -0,0 +1,16 @@
+import cv2 
+cap = cv2.VideoCapture(0)
+counter=0
+while True :
+    x,frame=cap.read()
+    #cv2.imshow('Image',frame) 
+    counter +=1
+    flipped=cv2.flip(frame,1)
+    
+    if counter%2 == 0 :
+        cv2.imshow('Image',frame)
+    else :
+        cv2.imshow('Image',flipped)
+
+    if cv2.waitKey(2000) & 0xFF == ord('q') :
+        break 
