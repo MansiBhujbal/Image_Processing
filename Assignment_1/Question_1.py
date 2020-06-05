c1c802bb64941cd838476bf966d28c5a96da71c9
MansiBhujbal <66293553+MansiBhujbal@users.noreply.github.com>
2020-06-06 00:14:07 +0530

    Update Question_1.py

--- Assignment_1/Question_1.py
+++ Assignment_1/Question_1.py
@@ -4,14 +4,12 @@ cap = cv2.VideoCapture(0)
 counter=0
 while True :
     x,frame=cap.read()
-    #cv2.imshow('Image',frame) 
+    cv2.imshow('Image',frame) 
     counter +=1
     flipped=cv2.flip(frame,0)
     
     if counter%5 == 0 :
         cv2.imshow('Image',flipped)
-    else :
-        cv2.imshow('Image',frame)
-
-    if cv2.waitKey(1000) & 0xFF == ord('q') :
+    
+    if cv2.waitKey(500) & 0xFF == ord('q') :
             break 
